dea8dbdb063bfe2a975d17c1727e6710c25f8c54
LaurinJ <laurin223@seznam.cz>
2021-04-19 23:27:34 +0200

    Product model

---
+++ models/product.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      min: 3,
+      max: 160,
+      required: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      index: true,
+    },
+    description: {
+      type: {},
+      required: true,
+      min: 100,
+      max: 2000000,
+    },
+    short_description: {
+      type: String,
+      max: 1000,
+    },
+    code: {
+      type: Number,
+    },
+    price: {
+      type: Number,
+    },
+    old_price: {
+      type: Number,
+    },
+    rating_sum: {
+      type: Number,
+      default: 0,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    categories: [{ type: ObjectId, ref: "Category", required: true }],
+    hidden: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", blogSchema);
